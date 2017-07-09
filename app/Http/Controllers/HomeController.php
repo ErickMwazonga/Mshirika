74406f10087469e8d79bb62880f00f86a6d5b824
Dianne <dianneprinsescah@gmail.com>
2017-07-09 08:59:02 +0300

    Rectifying Send Email listener

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+//use App\Events\SendMail;
 use App\Events\SendMail;
 use Illuminate\Http\Request;
 
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,7 +17,7 @@ class EventServiceProvider extends ServiceProvider
             'App\Listeners\EventListener',
         ],
         'App\Events\SendMail' => [
-            'App\Listeners\SendmailFired',
+            'App\Listeners\SendMailFired',
         ]
     ];
 
